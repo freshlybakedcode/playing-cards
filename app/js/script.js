f8366d297f8ef51251a6bc1370965f23f709fe48
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2018-02-11 23:40:41 +0000

    Added faceUp argument to deck creation. createUI renders front and backs

--- app/js/script.js
+++ app/js/script.js
@@ -3,14 +3,14 @@
 const Cards = {
     suits: ['clubs', 'diamonds', 'hearts', 'spades'],
     face: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
-    createDeck: function createDeck(deckArrayName) {
+    createDeck: function createDeck(deckArrayName, faceUp) {
         this.suits.forEach((suit) => {
             this.face.forEach((face, index) => {
                 const card = {
                     face,
                     suit,
                     value: index + 1,
-                    faceUp: true,
+                    faceUp,
                 }
                 deckArrayName.push(card);
             });
@@ -34,7 +34,7 @@ const Cards = {
         deck.forEach((element) => {
             // Create a new div element 
             const newCard = document.createElement("div");
-            let symbols = '';
+            let symbols = '';                   //Glyphs shown in middle of card
             if (element.value < 11) {           //Not a picture card
                 for (let i = 0; i < element.value; i++) {
                     let invertedModifier = 0;   //6 & 7 need a tweak to calculate which glyphs need inverting
@@ -52,13 +52,18 @@ const Cards = {
             } else if (element.value === 13) {
                 symbols = `<span class="king"></span>`;
             }
+            let faceUp = '';
+            element.faceUp ? faceUp = 'face-up' : faceUp = '';
             newCard.id = element.face + element.suit;
-            newCard.classList = `card _${element.face} ${element.suit}`;
+            newCard.classList = `card _${element.face} ${element.suit} ${faceUp}`;
             newCard.setAttribute('data-value', element.value);
             newCard.innerHTML = `
-                <span class="top">${element.face}</span>
-                <span class="middle">${symbols}</span>
-                <span class="bottom inverted">${element.face}</span>
+                <span class="card-front">
+                    <span class="top">${element.face}</span>
+                    <span class="middle">${symbols}</span>
+                    <span class="bottom inverted">${element.face}</span>
+                </span>
+                <span class="card-back"></span>
             `;
             // Insert it into defined div
             insertPosition.appendChild(newCard);
@@ -68,7 +73,7 @@ const Cards = {
 
 let deck = [];
 
-Cards.createDeck(deck);
+Cards.createDeck(deck, true);   //Create new deck, cards face up
 Cards.shuffleDeck(deck);
 Cards.createUI(deck, table)
 console.table(deck);
