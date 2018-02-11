1c3929561646c9188552be48869549a4e0764cce
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2018-02-11 22:12:29 +0000

    Refactored into single object. Moar ES6 to tidy and fix scoping errors

--- app/js/script.js
+++ app/js/script.js
@@ -1,71 +1,74 @@
 'use strict';
 
-let deck = [];
-const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-const face = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-
-const createDeck = function createDeck(deckArrayName) {
-    suits.forEach(function(suit) {
-        face.forEach(function(face, index) {
-            const card = {
-                face,
-                suit,
-                value: index + 1,
-            }
-            deckArrayName.push(card);
-        });
-    });
-};
-const shuffleDeck = function shuffleDeck(deck) {
-    let remainingElements = deck.length;
-    let temp;
-    let elementToMove;
-
-    while (remainingElements) {
-      // Pick a remaining element…
-      elementToMove = Math.floor(Math.random() * remainingElements--);
-      // And swap it with the current element.
-      temp = deck[remainingElements];
-      deck[remainingElements] = deck[elementToMove];
-      deck[elementToMove] = temp;
-    }
-};
-const createUI = function createUI(deck, insertPosition) {
-    deck.forEach(function(element) {
-        // Create a new div element 
-        const newCard = document.createElement("div");
-        let symbols = '';
-        if (element.value < 11) {
-            for (let i = 0; i < element.value; i++) {
-                let invertedModifier = 0;   //6 & 7 need a tweak to calculate which glyphs need inverting
-                element.value === 6 || element.value === 7 ? invertedModifier = 1 : invertedModifier = 0;
-                if (i >= Math.ceil(element.value / 2) + invertedModifier) {
-                    symbols += `<span class="inverted"></span>`;
-                } else {
-                    symbols += `<span></span>`;
+const Cards = {
+    suits: ['clubs', 'diamonds', 'hearts', 'spades'],
+    face: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+    createDeck: function createDeck(deckArrayName) {
+        this.suits.forEach((suit) => {
+            this.face.forEach((face, index) => {
+                const card = {
+                    face,
+                    suit,
+                    value: index + 1,
+                    faceUp: true,
                 }
-            };
-        } else if (element.value === 11) {
-            symbols = `<span class="jack"></span>`;
-        } else if (element.value === 12) {
-            symbols = `<span class="queen"></span>`;
-        } else if (element.value === 13) {
-            symbols = `<span class="king"></span>`;
+                deckArrayName.push(card);
+            });
+        });
+    },
+    shuffleDeck: function shuffleDeck(deck) {
+        let remainingElements = deck.length;
+        let temp;
+        let elementToMove;
+    
+        while (remainingElements) {
+          // Pick a remaining element…
+          elementToMove = Math.floor(Math.random() * remainingElements--);
+          // And swap it with the current element.
+          temp = deck[remainingElements];
+          deck[remainingElements] = deck[elementToMove];
+          deck[elementToMove] = temp;
         }
-        newCard.id = element.face + element.suit;
-        newCard.classList = `card _${element.face} ${element.suit}`;
-        newCard.setAttribute('data-value', element.value);
-        newCard.innerHTML = `
-            <span class="top">${element.face}</span>
-            <span class="middle">${symbols}</span>
-            <span class="bottom inverted">${element.face}</span>
-        `;
-        // Insert it into defined div
-        insertPosition.appendChild(newCard);
-    });
+    },
+    createUI: function createUI(deck, insertPosition) {
+        deck.forEach((element) => {
+            // Create a new div element 
+            const newCard = document.createElement("div");
+            let symbols = '';
+            if (element.value < 11) {           //Not a picture card
+                for (let i = 0; i < element.value; i++) {
+                    let invertedModifier = 0;   //6 & 7 need a tweak to calculate which glyphs need inverting
+                    element.value === 6 || element.value === 7 ? invertedModifier = 1 : invertedModifier = 0;
+                    if (i >= Math.ceil(element.value / 2) + invertedModifier) {
+                        symbols += `<span class="inverted"></span>`;
+                    } else {
+                        symbols += `<span></span>`;
+                    }
+                };
+            } else if (element.value === 11) {
+                symbols = `<span class="jack"></span>`;
+            } else if (element.value === 12) {
+                symbols = `<span class="queen"></span>`;
+            } else if (element.value === 13) {
+                symbols = `<span class="king"></span>`;
+            }
+            newCard.id = element.face + element.suit;
+            newCard.classList = `card _${element.face} ${element.suit}`;
+            newCard.setAttribute('data-value', element.value);
+            newCard.innerHTML = `
+                <span class="top">${element.face}</span>
+                <span class="middle">${symbols}</span>
+                <span class="bottom inverted">${element.face}</span>
+            `;
+            // Insert it into defined div
+            insertPosition.appendChild(newCard);
+        });
+    },
 };
 
-createDeck(deck);
-shuffleDeck(deck);
-createUI(deck, table)
+let deck = [];
+
+Cards.createDeck(deck);
+Cards.shuffleDeck(deck);
+Cards.createUI(deck, table)
 console.table(deck);
